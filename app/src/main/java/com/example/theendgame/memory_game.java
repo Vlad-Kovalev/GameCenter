b2acdfc246c5742933c4476d536e74a222bf90b3
Vlad-Kovalev <vlad.assembler@gmail.com>
2023-07-24 23:17:48 +0300

    Game center v1.07
    Memory game: made buttons unclickable after finding a pair
    made floating image & text when winning a game.

--- app/src/main/java/com/example/theendgame/memory_game.java
+++ app/src/main/java/com/example/theendgame/memory_game.java
@@ -7,7 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Arrays;
@@ -16,19 +18,23 @@ import java.util.Collections;
 public class memory_game extends AppCompatActivity {
 
     private TextView tvName;
-    private ImageView card1, card2, card3, card4, card5, card6, card7, card8, btnBack;
-    private ImageView firstFlippedCard;
-    private ImageView secondFlippedCard;
+    private GridLayout glCards;
+    private ImageView card1, card2, card3, card4, card5, card6, card7, card8, ivCongrats, btnBack;
+    private ImageView firstFlippedCard; private ImageView secondFlippedCard;
     private AppCompatButton btnRestart;
     private ImageView[] cards = new ImageView[8];
+    private int tryCount = 1;
+    private int matchedPairs = 0;
+    private String receivedName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_game);
         if (getSupportActionBar() != null) {getSupportActionBar().hide();}
-
-        tvName = findViewById(R.id.tvName); btnBack = findViewById(R.id.btnBack); btnRestart = findViewById(R.id.btnRestart);
+        Intent intent = getIntent();
+        receivedName = intent.getStringExtra("keyname");
+        tvName = findViewById(R.id.tvName); btnBack = findViewById(R.id.btnBack); btnRestart = findViewById(R.id.btnRestart); ivCongrats = findViewById(R.id.ivCongrats); glCards = findViewById(R.id.glCards);
         card1 = findViewById(R.id.card1); card2 = findViewById(R.id.card2); card3 = findViewById(R.id.card3); card4 = findViewById(R.id.card4); card5 = findViewById(R.id.card5); card6 = findViewById(R.id.card6); card7 = findViewById(R.id.card7); card8 = findViewById(R.id.card8);
         cards[0] = findViewById(R.id.card1); cards[1] = findViewById(R.id.card2); cards[2] = findViewById(R.id.card3); cards[3] = findViewById(R.id.card4); cards[4] = findViewById(R.id.card5); cards[5] = findViewById(R.id.card6); cards[6] = findViewById(R.id.card7); cards[7] = findViewById(R.id.card8);
         Collections.shuffle(Arrays.asList(cards));
@@ -41,7 +47,6 @@ public class memory_game extends AppCompatActivity {
         cards[5].setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {flipCard(cards[5]);}});
         cards[6].setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {flipCard(cards[6]);}});
         cards[7].setOnClickListener(new View.OnClickListener() {@Override public void onClick(View v) {flipCard(cards[7]);}});
-
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,23 +62,39 @@ public class memory_game extends AppCompatActivity {
             }
         });
 
-        Intent intent = getIntent();
-        String recivedName = intent.getStringExtra("keyname");
-        TextView textView = findViewById(R.id.tvName);
-        textView.setText("Hello " + recivedName);
     }
+
     private void flipCard(ImageView card) {
+        updateTryCount();
         if (firstFlippedCard == null) {
             firstFlippedCard = card;
             card.setImageResource(getImageResource(card));
+            tryCount++;
         }
         else if (secondFlippedCard == null) {
             secondFlippedCard = card;
             card.setImageResource(getImageResource(card));
 
             if (getImageResource(firstFlippedCard) == getImageResource(secondFlippedCard)) {
+                firstFlippedCard.setOnClickListener(null);
+                secondFlippedCard.setOnClickListener(null);
+
                 firstFlippedCard = null;
                 secondFlippedCard = null;
+                tryCount++;
+
+                matchedPairs++;
+                if (matchedPairs == 4) {
+                    glCards.setVisibility(View.GONE);
+                    ivCongrats.setVisibility(View.VISIBLE);
+                    tvName.setVisibility(View.VISIBLE);
+                    tryCount--;
+                    int heightInDp = 55;
+                    float scale = getResources().getDisplayMetrics().density;
+                    int heightInPixels = (int) (heightInDp * scale + 0.5f);
+                    tvName.getLayoutParams().height = heightInPixels; tvName.invalidate();
+                    tvName.setText("Congratulations " + receivedName + "!\n" + "you won in " + tryCount + " turns");
+                }
             } else {
                 Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
@@ -83,6 +104,7 @@ public class memory_game extends AppCompatActivity {
                         secondFlippedCard.setImageResource(R.drawable.card_back);
                         firstFlippedCard = null;
                         secondFlippedCard = null;
+                        tryCount++;
                     }
                 }, 1000);
             }
@@ -100,5 +122,11 @@ public class memory_game extends AppCompatActivity {
         else if (card == cards[6] || card == cards[7]) {
             return R.drawable.fourthpic;}
         return R.drawable.card_back;
+    }
+
+    private void updateTryCount() {
+        tvName.setVisibility(View.VISIBLE);
+        if (tryCount == 1) tvName.setText(receivedName + " you clicked " + tryCount + " time");
+        else tvName.setText(receivedName + " you clicked " + tryCount + " times");
     }
 }
